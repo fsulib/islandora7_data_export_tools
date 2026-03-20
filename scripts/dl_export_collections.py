3a3bdf50e1e5c6501251b900a62967ad88e8227f
Bryan J. Brown <bjbrown@fsu.edu>
2026-03-20 20:21:41 +0000

    Uses black to reformat dl_export_collections.py

--- scripts/dl_export_collections.py
+++ scripts/dl_export_collections.py
@@ -23,84 +23,152 @@ collections_with_noncollection_children = []
 
 # Functions
 def log(message):
-  formatted_message = "{} {}\n".format(datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S"), message)
-  print(formatted_message)
-  logfile = open("{}{}.log".format(output_path, logtime), "a")
-  logfile.write(formatted_message)
+    formatted_message = "{} {}\n".format(
+        datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S"), message
+    )
+    print(formatted_message)
+    logfile = open("{}{}.log".format(output_path, logtime), "a")
+    logfile.write(formatted_message)
+
 
 def get_collection_pid_from_path(collection_pid_path):
-  return collection_pid_path.split("/")[-1]
-  
+    return collection_pid_path.split("/")[-1]
+
+
 def get_collection_directory_path(collection_pid_path):
-  return output_path + collection_pid_path.replace(':', '_')
+    return output_path + collection_pid_path.replace(":", "_")
+
 
 def get_collection_file_prefix(collection_pid_path):
-  return get_collection_pid_from_path(collection_pid_path).replace(':', '_')
+    return get_collection_pid_from_path(collection_pid_path).replace(":", "_")
+
 
 def write_file_to_collection_directory(collection_pid_path, filename, data):
-  collection_directory = get_collection_directory_path(collection_pid_path)
-  log("Writing file {} to {}.".format(filename, collection_directory))
-  if not os.path.isdir(collection_directory):
-    os.mkdir(collection_directory)
-  file = open("{}/{}".format(collection_directory, filename), "a")
-  file.write(data)
+    collection_directory = get_collection_directory_path(collection_pid_path)
+    log("Writing file {} to {}.".format(filename, collection_directory))
+    if not os.path.isdir(collection_directory):
+        os.mkdir(collection_directory)
+    file = open("{}/{}".format(collection_directory, filename), "a")
+    file.write(data)
+
 
 def get_collection_children_pids(collection_pid_path):
-  child_data = {}
-  collection_pid = collection_pid_path.split("/")[-1]
-  child_data['collections'] = subprocess.run(["drush -u 1 islandora_datastream_crud_fetch_pids --content_model=islandora:collectionCModel --collection={}".format(collection_pid)], shell=True, capture_output=True, text=True).stdout.splitlines()
-  child_data['noncollections'] = subprocess.run(["drush -u 1 islandora_datastream_crud_fetch_pids --without_cmodel=islandora:collectionCModel --collection={}".format(collection_pid)], shell=True, capture_output=True, text=True).stdout.splitlines()
-  if child_data['noncollections']:
-    log("{} contains noncollection children.".format(collection_pid_path))
-    collections_with_noncollection_children.append(collection_pid_path)
-  return child_data
+    child_data = {}
+    collection_pid = collection_pid_path.split("/")[-1]
+    child_data["collections"] = subprocess.run(
+        [
+            "drush -u 1 islandora_datastream_crud_fetch_pids --content_model=islandora:collectionCModel --collection={}".format(
+                collection_pid
+            )
+        ],
+        shell=True,
+        capture_output=True,
+        text=True,
+    ).stdout.splitlines()
+    child_data["noncollections"] = subprocess.run(
+        [
+            "drush -u 1 islandora_datastream_crud_fetch_pids --without_cmodel=islandora:collectionCModel --collection={}".format(
+                collection_pid
+            )
+        ],
+        shell=True,
+        capture_output=True,
+        text=True,
+    ).stdout.splitlines()
+    if child_data["noncollections"]:
+        log("{} contains noncollection children.".format(collection_pid_path))
+        collections_with_noncollection_children.append(collection_pid_path)
+    return child_data
+
 
 def export_collection(collection_pid_path):
-  collection_directory = get_collection_directory_path(collection_pid_path)
-  collection_file_prefix = get_collection_file_prefix(collection_pid_path)
-  collection_pid = get_collection_pid_from_path(collection_pid_path)
+    collection_directory = get_collection_directory_path(collection_pid_path)
+    collection_file_prefix = get_collection_file_prefix(collection_pid_path)
+    collection_pid = get_collection_pid_from_path(collection_pid_path)
+    log("Beginning export of {}.".format(collection_pid_path))
+    write_file_to_collection_directory(
+        collection_pid_path, "{}.pid".format(collection_file_prefix), collection_pid
+    )
+    collection_child_pids = get_collection_children_pids(collection_pid_path)
+    if collection_child_pids["collections"]:
+        write_file_to_collection_directory(
+            collection_pid_path,
+            "{}.child-collections.pids".format(collection_file_prefix),
+            "\n".join(collection_child_pids["collections"]),
+        )
+        for child_collection in collection_child_pids["collections"]:
+            collections_to_process.append(
+                "{}/{}".format(collection_pid_path, child_collection)
+            )
+    if collection_child_pids["noncollections"]:
+        write_file_to_collection_directory(
+            collection_pid_path,
+            "{}.child-noncollections.pids".format(collection_file_prefix),
+            "\n".join(collection_child_pids["noncollections"]),
+        )
+    subprocess.run(
+        [
+            "drush -u 1 -y islandora_datastream_crud_fetch_datastreams --pid_file={0}/{1}.pid --dsid=RELS-EXT --datastreams_directory={0}".format(
+                collection_directory, collection_file_prefix
+            )
+        ],
+        shell=True,
+        capture_output=True,
+        text=True,
+    ).stdout.splitlines()
+    subprocess.run(
+        [
+            "drush -u 1 -y islandora_datastream_crud_fetch_datastreams --pid_file={0}/{1}.pid --dsid=DC --datastreams_directory={0}".format(
+                collection_directory, collection_file_prefix
+            )
+        ],
+        shell=True,
+        capture_output=True,
+        text=True,
+    ).stdout.splitlines()
+    subprocess.run(
+        [
+            "drush -u 1 -y islandora_datastream_crud_fetch_datastreams --pid_file={0}/{1}.pid --dsid=MODS --datastreams_directory={0}".format(
+                collection_directory, collection_file_prefix
+            )
+        ],
+        shell=True,
+        capture_output=True,
+        text=True,
+    ).stdout.splitlines()
+    collections_to_process.remove(collection_pid_path)
+    collections_processed.append(collection_pid_path)
+    log("Finished export of {}.".format(collection_pid_path))
+    if collections_to_process:
+        export_collection(collections_to_process[0])
 
-  log("Beginning export of {}.".format(collection_pid_path))
-  write_file_to_collection_directory(collection_pid_path, "{}.pid".format(collection_file_prefix), collection_pid)
-
-  collection_child_pids = get_collection_children_pids(collection_pid_path)
-  if collection_child_pids['collections']:
-    write_file_to_collection_directory(collection_pid_path, "{}.child-collections.pids".format(collection_file_prefix), "\n".join(collection_child_pids['collections']))
-    for child_collection in collection_child_pids['collections']:
-      collections_to_process.append("{}/{}".format(collection_pid_path, child_collection))
-  if collection_child_pids['noncollections']:
-    write_file_to_collection_directory(collection_pid_path, "{}.child-noncollections.pids".format(collection_file_prefix), "\n".join(collection_child_pids['noncollections']))
-
-  subprocess.run(["drush -u 1 -y islandora_datastream_crud_fetch_datastreams --pid_file={0}/{1}.pid --dsid=RELS-EXT --datastreams_directory={0}".format(collection_directory, collection_file_prefix)], shell=True, capture_output=True, text=True).stdout.splitlines()
-
-  subprocess.run(["drush -u 1 -y islandora_datastream_crud_fetch_datastreams --pid_file={0}/{1}.pid --dsid=DC --datastreams_directory={0}".format(collection_directory, collection_file_prefix)], shell=True, capture_output=True, text=True).stdout.splitlines()
-
-  subprocess.run(["drush -u 1 -y islandora_datastream_crud_fetch_datastreams --pid_file={0}/{1}.pid --dsid=MODS --datastreams_directory={0}".format(collection_directory, collection_file_prefix)], shell=True, capture_output=True, text=True).stdout.splitlines()
-
-  collections_to_process.remove(collection_pid_path)
-  collections_processed.append(collection_pid_path)
-  log("Finished export of {}.".format(collection_pid_path))
-  if collections_to_process:
-    export_collection(collections_to_process[0])
 
 def write_collections_with_noncollection_children_list():
-  if collections_with_noncollection_children:
-    formatted_data =  "\n".join(collections_with_noncollection_children)
-    log("The following collections with noncollection children were detected and saved to root/collections_with_noncollection_children.txt:\n{}".format(formatted_data))
-    file = open("{}/collections_with_noncollection_children.txt".format(output_path), "a")
-    file.write(formatted_data)
-  else:
-    log("No collections with noncollection children were detected.")
+    if collections_with_noncollection_children:
+        formatted_data = "\n".join(collections_with_noncollection_children)
+        log(
+            "The following collections with noncollection children were detected and saved to root/collections_with_noncollection_children.txt:\n{}".format(
+                formatted_data
+            )
+        )
+        file = open(
+            "{}/collections_with_noncollection_children.txt".format(output_path), "a"
+        )
+        file.write(formatted_data)
+    else:
+        log("No collections with noncollection children were detected.")
 
 
 # Main
 starttime = int(time.time())
 log("Beginning collection data export.")
-
-export_collection('fsu:digital_library')
-
+export_collection("fsu:digital_library")
 log("Finished collection data export.")
 write_collections_with_noncollection_children_list()
 endtime = int(time.time())
 totaltime = endtime - starttime
-log("Export took {} seconds to process {} collections: \n{}".format(totaltime, len(collections_processed), "\n".join(collections_processed)))
+log(
+    "Export took {} seconds to process {} collections: \n{}".format(
+        totaltime, len(collections_processed), "\n".join(collections_processed)
+    )
+)
